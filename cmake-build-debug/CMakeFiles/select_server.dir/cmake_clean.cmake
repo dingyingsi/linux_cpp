bc664a72f4da24a33941c02dd38b8d3c87f11898
dingyingsi <dingyingsi@126.com>
2020-05-06 23:22:29 +0800

    doc

---
+++ cmake-build-debug/CMakeFiles/select_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/select_server.dir/select/server.c.o"
+  "select_server"
+  "select_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/select_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
