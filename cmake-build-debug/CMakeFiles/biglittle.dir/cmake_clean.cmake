90bf8c327e671b15c5b978f317ef526b6351bb34
dingyingsi <dingyingsi@126.com>
2020-05-06 23:39:30 +0800

    doc

---
+++ cmake-build-debug/CMakeFiles/biglittle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/biglittle.dir/biglittle/biglitter.c.o"
+  "biglittle"
+  "biglittle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/biglittle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
