8aaf5075316249f6424b3cba26b28fe6ae8d7288
dingyingsi <dingyingsi@126.com>
2020-05-07 00:38:13 +0800

    doc

---
+++ cmake-build-debug/CMakeFiles/addr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/addr.dir/addr/addr.c.o"
+  "addr"
+  "addr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/addr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/socket.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/socket.dir/socket/socket.c.o"
+  "socket"
+  "socket.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/socket.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
