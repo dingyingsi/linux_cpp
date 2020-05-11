344a7c97f767a8a3ac9ed85ce98b19ded8fd9723
dingyingsi <dingyingsi@126.com>
2020-05-11 13:45:21 +0800

    doc

---
+++ cmake-build-debug/CMakeFiles/p2p_client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p2p_client.dir/p2p/p2p_client.c.o"
+  "p2p_client"
+  "p2p_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/p2p_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/p2p_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p2p_server.dir/p2p/p2p_server.c.o"
+  "p2p_server"
+  "p2p_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/p2p_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
