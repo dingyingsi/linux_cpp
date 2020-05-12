3d1bce2f51c3fb9f2c47a0b0b3856f31ece9c2ca
dingyingsi <dingyingsi@126.com>
2020-05-12 14:42:42 +0800

    doc

---
+++ cmake-build-debug/CMakeFiles/echo2_fork_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/echo2_fork_server.dir/echo2_fork/echo2_fork_server.c.o"
+  "echo2_fork_server"
+  "echo2_fork_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/echo2_fork_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
