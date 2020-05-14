0cba78b5644e0fbcc71728b86d18ea7c5027f21f
dingyingsi <dingyingsi@126.com>
2020-05-14 23:33:39 +0800

    doc

---
+++ cmake-build-debug/CMakeFiles/gethostbyname.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gethostbyname.dir/gethostbyname/gethostbyname.c.o"
+  "gethostbyname"
+  "gethostbyname.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gethostbyname.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/gethostbyname2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gethostbyname2.dir/gethostbyname/gethostbyname2.c.o"
+  "gethostbyname2"
+  "gethostbyname2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gethostbyname2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
