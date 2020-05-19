54ecbc843a68ca16c990f680846cf7861fef992f
dingyingsi <dingyingsi@126.com>
2020-05-19 08:35:20 +0800

    doc

---
+++ cmake-build-debug/CMakeFiles/echo7_fork_client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/echo7_fork_client.dir/echo7_fork/echo7_fork_client.c.o"
+  "echo7_fork_client"
+  "echo7_fork_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/echo7_fork_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
