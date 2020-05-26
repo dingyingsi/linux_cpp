e9b01d82277e099251d9604aec0a0a53e7a76c6b
dingyingsi <dingyingsi@126.com>
2020-05-26 22:39:43 +0800

    doc

---
+++ cmake-build-debug/CMakeFiles/echo_udp_client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/echo_udp_client.dir/echo_udp/echo_udp_client.c.o"
+  "echo_udp_client"
+  "echo_udp_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/echo_udp_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
