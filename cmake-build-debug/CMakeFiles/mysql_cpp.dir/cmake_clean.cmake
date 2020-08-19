45553ddfb6a40a939f895eab509650f4b31dcd28
dingyingsi <dingyingsi@126.com>
2020-08-19 15:07:10 +0800

    linux_cpp  mysql

---
+++ cmake-build-debug/CMakeFiles/mysql_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mysql_cpp.dir/mysql/mysql_cpp.cpp.o"
+  "mysql_cpp"
+  "mysql_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mysql_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
