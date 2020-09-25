34d9ddd376b32eb5cba9841a51ba0886d23ee9cb
dingyingsi <dingyingsi@126.com>
2020-09-25 10:44:04 +0800

    linux_cpp  mysql

--- main.cpp
+++ main.cpp
@@ -3,23 +3,7 @@
 
 using namespace std;
 
-
-struct Person {
-
-    int b[5];
-    int c;
-};
-
 int main(int argc, char **argv) {
-
-    Person person;
-
-    person.b[1] = 1;
-    person.c = 10;
-
-    cout << sizeof(double ) << endl;
-
-    cout << sizeof(Person) << endl;
 
     return EXIT_SUCCESS;
 }
